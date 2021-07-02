3c4961a6d99a6e3caf038a79bb402e6946fbad1d
Natanael Lima <taelima1997@gmail.com>
2021-07-02 12:53:01 -0300

    add config docker hosts

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,3 +3,15 @@ import { createConnections } from 'typeorm';
 createConnections()
   .then(() => console.log('connection database success'))
   .catch(err => console.log('connection database failure :=>=>', err));
+
+// interface IOptions {
+//   host: string;
+// }
+
+// getConnectionOptions().then(options => {
+//   const newOptions = options as IOptions;
+//   newOptions.host = 'mysql'; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+//   createConnection({ ...options })
+//     .then(() => console.log('connection database mysql 🎉'))
+//     .catch(err => console.log('connection database mysql fail ', err));
+// });

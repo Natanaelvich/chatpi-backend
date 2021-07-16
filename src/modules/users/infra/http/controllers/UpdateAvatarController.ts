33d1020190b2cf7993c4971ffd8dfd22ab21960c
Natanael Lima <taelima1997@gmail.com>
2021-07-16 16:56:28 -0300

    add log file

--- src/modules/users/infra/http/controllers/UpdateAvatarController.ts
+++ src/modules/users/infra/http/controllers/UpdateAvatarController.ts
@@ -5,6 +5,7 @@ import { container } from 'tsyringe';
 
 export default class UpdateAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
+    console.log(request.file);
     const updateUserAvatar = container.resolve(UpdateUserAvatarService);
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,

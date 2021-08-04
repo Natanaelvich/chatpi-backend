56a199d02d4df6049cd455834936630cb0f372a8
Natanael Lima <taelima1997@gmail.com>
2021-08-04 19:08:17 -0300

    update UserTokenRepository with new interface

--- src/modules/users/infra/typeorm/repositories/UserTokenRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UserTokenRepository.ts
@@ -9,6 +9,28 @@ class UserTokenRepository implements IUserTokenRepository {
     this.ormRepository = getRepository(UserToken);
   }
 
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string,
+  ): Promise<UserToken | undefined> {
+    const userTokens = await this.ormRepository.findOne({
+      user_id,
+      refresh_token,
+    });
+    return userTokens;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.ormRepository.delete(id);
+  }
+
+  async findByRefreshToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.ormRepository.findOne({
+      refresh_token: token,
+    });
+    return userToken;
+  }
+
   public async findByToken(token: string): Promise<UserToken | undefined> {
     const findUserToken = await this.ormRepository.findOne({
       where: { token },

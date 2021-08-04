708cc8b811da41303a72745affb8fe4a278dfa33
Natanael Lima <taelima1997@gmail.com>
2021-08-04 19:07:54 -0300

    update with methos refresh token

--- src/modules/users/repositories/IUserTokenRepository.ts
+++ src/modules/users/repositories/IUserTokenRepository.ts
@@ -3,4 +3,10 @@ import UserToken from '../infra/typeorm/entities/UserToken';
 export default interface IUserTokenRepository {
   generate(user_id: string): Promise<UserToken>;
   findByToken(token: string): Promise<UserToken | undefined>;
+  findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string,
+  ): Promise<UserToken | undefined>;
+  deleteById(id: string): Promise<void>;
+  findByRefreshToken(token: string): Promise<UserToken | undefined>;
 }

8c6d78db5752c077b4d185495625c84f0e5566d5
Natanael Lima <taelima1997@gmail.com>
2021-08-05 23:55:25 -0300

    update jwt config

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,5 +2,8 @@ export default {
   jwt: {
     secret: process.env.APP_SECRET || 'secret-fake',
     exp: '1d',
+    secret_refresh_token: process.env.APP_SECRET || 'secret-fake',
+    expires_in_refresh_token: '30d',
+    expires_refresh_token_days: 30,
   },
 };

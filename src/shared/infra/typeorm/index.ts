44a85fd3aebfe9f95b052fb032db46b22829f5ea
Natanael Lima <taelima1997@gmail.com>
2021-08-06 18:16:43 -0300

    update with env dev true

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,17 +1,23 @@
-import { createConnections } from 'typeorm';
+import {
+  createConnection,
+  createConnections,
+  getConnectionOptions,
+} from 'typeorm';
 
-createConnections()
-  .then(() => console.log('connection database success'))
-  .catch(err => console.log('connection database failure :=>=>', err));
+if (process.env.TS_NODE_DEV) {
+  interface IOptions {
+    host: string;
+  }
 
-// interface IOptions {
-//   host: string;
-// }
-
-// getConnectionOptions().then(options => {
-//   const newOptions = options as IOptions;
-//   newOptions.host = 'mysql'; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-//   createConnection({ ...options })
-//     .then(() => console.log('connection database mysql 🎉'))
-//     .catch(err => console.log('connection database mysql fail ', err));
-// });
+  getConnectionOptions().then(options => {
+    const newOptions = options as IOptions;
+    newOptions.host = 'mysql'; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+    createConnection({ ...options })
+      .then(() => console.log('connection database mysql 🎉'))
+      .catch(err => console.log('connection database mysql fail ', err));
+  });
+} else {
+  createConnections()
+    .then(() => console.log('connection database success'))
+    .catch(err => console.log('connection database failure:', err));
+}

5c0d429c1060edb0807f7958833bc1a0f4bd1088
Natanael Lima <taelima1997@gmail.com>
2021-08-07 22:16:14 -0300

    update tests authenticate user

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,20 +1,28 @@
+import DayjsDateProvider from '@shared/container/providers/DateProvider/implementations/DayjsDateProvider';
 import AppError from '@shared/errors/AppError';
 import FakeBCryptHashProvider from '../providers/HashProvider/fakes/FakeBCryptHashProvider copy';
 import FakeUserRepository from '../repositories/fakes/FakeUserRepository';
+import FakeUserTokenRepository from '../repositories/fakes/FakeUserTokenRepository';
 import AuthenticateUserService from './AuthenticateUserService';
 
 let fakeUserRepository: FakeUserRepository;
+let fakeUserTokenRepository: FakeUserTokenRepository;
 let fakeBCryptHashProvider: FakeBCryptHashProvider;
+let dateProvider: DayjsDateProvider;
 let authenticateUser: AuthenticateUserService;
 
 describe('Autheticate user', () => {
   beforeEach(() => {
     fakeUserRepository = new FakeUserRepository();
+    fakeUserTokenRepository = new FakeUserTokenRepository();
     fakeBCryptHashProvider = new FakeBCryptHashProvider();
+    dateProvider = new DayjsDateProvider();
 
     authenticateUser = new AuthenticateUserService(
       fakeUserRepository,
+      fakeUserTokenRepository,
       fakeBCryptHashProvider,
+      dateProvider,
     );
   });
 
@@ -32,6 +40,8 @@ describe('Autheticate user', () => {
     });
 
     expect(response).toHaveProperty('token');
+    expect(response).toHaveProperty('refresh_token');
+    expect(response).toHaveProperty('user');
     expect(response.user).toEqual(user);
   });
 

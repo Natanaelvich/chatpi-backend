e728bb6c798b417aeb58931a4565b0de42fab952
Natanael Lima <taelima1997@gmail.com>
2021-08-14 11:27:16 -0300

    replace userUpdateController to ProfileController

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -4,14 +4,14 @@ import { classToClass } from 'class-transformer';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
-export default class UsersUpdateController {
+export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const showUserService = container.resolve(ShowUserService);
 
     const user = await showUserService.execute({ user_id });
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -28,6 +28,6 @@ export default class UsersUpdateController {
       old_password,
     });
 
-    return response.json({ user: classToClass(user) });
+    return response.json(classToClass(user));
   }
 }

a0a2911a8a3b866eee3d3dca4aca2d9f4bb68f94
Natanael Lima <taelima1997@gmail.com>
2021-08-14 11:27:33 -0300

    add validation on update user

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,14 +1,26 @@
+import { celebrate, Joi, Segments } from 'celebrate';
 import { Router } from 'express';
-import UsersUpdateController from '../controllers/UsersUpdateController';
+import ProfileController from '../controllers/ProfileController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
-const userUpdateController = new UsersUpdateController();
+const profileController = new ProfileController();
 
 const profileRouter = Router();
 
 profileRouter.use(ensureAuthenticated);
 
-profileRouter.put('/update', userUpdateController.update);
-profileRouter.get('/', userUpdateController.show);
+profileRouter.put(
+  '/update',
+  celebrate({
+    [Segments.BODY]: Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+      name: Joi.string().required(),
+      old_password: Joi.string().required(),
+    }),
+  }),
+  profileController.update,
+);
+profileRouter.get('/', profileController.show);
 
 export default profileRouter;

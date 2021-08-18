35bb65832201d8d715aec958feac4e339dfd4538
Natanael Lima <taelima1997@gmail.com>
2021-08-17 22:49:50 -0300

    update resfresh token exp

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,7 @@
 export default {
   jwt: {
     secret: process.env.APP_SECRET || 'secret-fake',
-    exp: '1d',
+    exp: '5s',
     secret_refresh_token: process.env.APP_SECRET || 'secret-fake',
     expires_in_refresh_token: '30d',
     expires_refresh_token_days: 30,
